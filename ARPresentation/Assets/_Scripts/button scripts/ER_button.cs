5fd9df9539bcda671018a3bc0cacca4a8612a0e2
calaw <calaw@ncsu.edu>
2018-04-22 20:47:02 -0400

    added navigation ring scene and assets to ARPresentation

---
+++ ARPresentation/Assets/_Scripts/button scripts/ER_button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ER_button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClick()
+    {
+        GetComponent<NavScript>().StartRotating(142);
+        Debug.Log("ER clicked");
+    }
+
+}
